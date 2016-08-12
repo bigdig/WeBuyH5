dae48493b7f82f858d5acb3a1b74abab6a0164d8
renxingguo <kastolo@gmail.com>
2016-08-12 11:39:36 +0800

    完成订单显示

--- module/order.js
+++ module/order.js
@@ -1,51 +1,87 @@
 
-mui.ajax(url, {
-    data: {
-        userid: userid,
-        type: '1'
-    },
-    dataType: 'JSONP',//服务器返回json格式数据
-    type: 'get',//HTTP请求类型
-    timeout: 10000,//超时时间设置为10秒；
-    success: function (data) {
-        //服务器返回响应，根据响应结果，分析是否登录成功；
-        var result = eval(data);
-        result = eval(result);
-        if (result != null) {
-            var x = 0;
-            var value = result[0].value;
-            var hcount = result[0].count;
-            var appsize = result[0].appsize;
-            mui('#pullrefresh').pullRefresh().endPullupToRefresh((++count > value)); //参数为true代表没有更多数据了。
-            var table = document.body.querySelector('.mui-table-view');
-            var cells = document.body.querySelectorAll('.mui-table-view-cell');
-            if (hcount > cells.length) {
-                for (var i = cells.length, len = i + appsize; i < len; i++) {
-                    var li = document.createElement('li');
-                    li.className = 'mui-table-view-cell';
-                    /*var contacts = result[0].page[i].contacts*/
-                    /*console.log(result[0].page[0].contacts)*/
-                    var contacts = result[0].page[i].contacts
-                    var contact;
-                    var divp;
-                    if (contacts == 1)
-                        contact = "<a id=" + result[0].page[i].id + " class='mui-btn mui-btn-bule'>企业联系人</a>"
-                    if (contacts == 0)
-                        contact = "<a id=" + result[0].page[i].id + " class='mui-btn mui-btn-col'>企业联系人</a>"
-                    if (contacts == 1)
-                        divp = "<p class='mui-pull-left pl-bule'>企业联系人</p>"
-                    if (contacts == 0)
-                        divp = ""
-                    li.innerHTML = '<div class="mui-slider-right mui-disabled" id="">' + contact + '<a id="' + result[0].page[i].id + '" class="mui-btn mui-btn-red">移除</a></div><div class="mui-slider-handle"><img class="mui-media-object mui-pull-left radius" src="/' + result[0].page[i].headimg + '"><div class="mui-media-body"><div class="mui-pull-left mui-ellipsis pl-name">' + result[0].page[i].nickname + '</div>' + divp + '</div><div class="mui-media-body"><p class="mui-pull-left">' + result[0].page[i].updatetime + '</p></div></div>'
-                    table.appendChild(li);
-                }
-            }
 
+
+var oShareStateList =  document.getElementById('item1'),
+    tmp = [
+                        /*'    <div class="mui-card">',*/
+                        '        <div class="mui-card-header mui-card-media">',
+                        '            <div class="mui-media-body">',
+                        '                <label class="lable-title">定单号：{origOrder}</label>',
+                        '                <label class="lable-state">{order.stateShow}</label>',
+                        '            </div>',
+                        '        </div>',
+                        '        <div class="mui-card-content">',
+                        '            <ul class="mui-table-view">',
+                        '                <li class="mui-table-view-cell mui-media">',
+                        '                    <a href="javascript:;">',
+                        '                        <img class="mui-pull-left" id="order-picture" src="http:/st.dressbook.cn/{wdGoodsSet.pic}">',
+                        '                        <div class="mui-media-body" id="order-desc">',
+                        '                            <h4 class="mui-ellipsis" style="line-height:1.5">{wdGoodsSet.title}</h4>',
+                        '                            <p class="mui-ellipsis" style="line-height:2">{wdGoodsSet.descr}</p>',
+                        '                            <p class="mui-ellipsis" style="line-height:2">{wdGoodsSet.descr}</p>',
+                        '                        </div>',
+                        '                    </a>',
+                        '                </li>',
+                        '            </ul>',
+                        '       </div>',
+                        '        <div class="mui-card-footer">',
+                        '            <button type="button" class="mui-btn mui-btn-outlined">取消定单</button>',
+                        '            <button type="button" class="mui-btn mui-btn-outlined mui-btn-danger">&nbsp;&nbsp;付&nbsp;款&nbsp;&nbsp;</button>',
+                        '        </div>'
+            ].join('');
+
+function fetchData(){
+
+    //异步获取数据
+
+  //http://ifc.dressbook.cn/wdOrdersGet.json?user_id=1095550
+    let dbkUrl = "http://ifc.dressbook.cn";
+
+    $.ajax({
+        url: dbkUrl + "/wdOrdersGet.jsonp",
+        data: "user_id=1095550",
+        type: "GET",
+        async: true,
+        dataType: 'JSONP',
+        success: function (data) {
+            console.log(JSON.stringify(data));
+            if (data.code == 1) {
+                renderAll( data.info.buyOrders );
+            }     
         }
+    });
+    
 
-    },
-    error: function (xhr, type, errorThrown) {
-        //异常处理；
-        console.log("没网了~~~~~~或者服务器断了~~");
+}
+
+function renderAll( datas ){
+    var data;
+    while( data = datas.shift() ){
+        render( data );
     }
-});
+}    
+
+function render( data ){
+
+    var _dom = document.createElement( 'div' );
+    _dom.setAttribute( 'class', "mui-card" );
+    _dom.innerHTML = tmp.replace( /(\{.+?\})/g, function($1){
+        var layer = $1.slice( 1, $1.length-1 ).split('.');
+        if(layer.length==1)
+            return data[ layer[0] ];
+        if(layer.length==2)
+            return data[ layer[0] ][ layer[1] ];
+        
+    } );
+    document.getElementById('cards-all').appendChild( _dom );
+    //
+    if( data.order.stateShow == "待付款")
+        document.getElementById('cards-topay').appendChild( _dom );
+
+    if( data.order.stateShow == "待发货")
+        document.getElementById('cards-toship').appendChild( _dom );
+
+    if( data.order.stateShow == "待收货")
+        document.getElementById('cards-shipping').appendChild( _dom );
+
+}
